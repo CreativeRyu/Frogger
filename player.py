650cdc344b63977e68edf380ded4f509869720c0
CreativeRyu <Creative.Ryu@web.de>
2023-04-13 23:46:59 +0200

    created walking assets and first basic animation

--- player.py
+++ player.py
@@ -3,20 +3,31 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, position, group):
         super().__init__(group)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill("White")
+        self.import_assets()
+        self.frame_index = 0
+        self.image = self.animation[self.frame_index]
         self.rect = self.image.get_rect(center = position)
         
         # Float Based Position
         self.position = pygame.math.Vector2(self.rect.center)
         self.direction = pygame.math.Vector2()
         self.speed = 200
+    
+    def import_assets(self):
+        path = "Player/Sidewalk/Walking Side_Animation 1_"
+        self.animation = [pygame.image.load(f"{path}{frame}.png").convert_alpha() for frame in range(10)]
         
     def move(self, delta_time):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
         self.position += self.direction * self.speed * delta_time
         self.rect.center = round(self.position.x), round(self.position.y)
+    
+    def animate(self, delta_time):
+        self.frame_index += 10 * delta_time
+        if self.frame_index >= len(self.animation):
+            self.frame_index = 0
+        self.image = self.animation[int(self.frame_index)]
     
     def handle_inputs(self):
         keys = pygame.key.get_pressed()
@@ -36,3 +47,4 @@ class Player(pygame.sprite.Sprite):
     def update(self, delta_time):
         self.handle_inputs()
         self.move(delta_time)
+        self.animate(delta_time)
