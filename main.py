736cf1fdd589f4a38716f4b4b98573d37e094952
Ryu_Ronin <creative.ryu@web.de>
2023-04-24 12:09:50 +0200

    received some graphics changed order structure

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import game_settings as gs
 from player import Player
 from car import Car
 
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+
+        
+    def custom_draw(self):
+        
+        # blit background first
+        
+        for sprite in self.sprites():
+            offset_position = sprite.rect.topleft + self.offset
+            display.blit(sprite, offset_position)
+
 # Basic Setup # # # # # # # # # # # # # 
 pygame.init()
 display = pygame.display.set_mode((gs.WINDOW_WIDTH, gs.WINDOW_HEIGHT))
@@ -10,11 +24,10 @@ pygame.display.set_caption("Frogger")
 clock = pygame.time.Clock()
 
 # Sprite Groups # # # # # # # # # # # #
-player_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
 # Object Declaration
-player = Player((gs.WINDOW_WIDTH // 2, gs.WINDOW_HEIGHT // 2), player_group)
+player = Player((gs.WINDOW_WIDTH // 2, gs.WINDOW_HEIGHT // 2), all_sprites)
 car = Car((100,200), all_sprites)
 
 # Game Loop # # # # # # # # # # # # # # # #
@@ -29,12 +42,10 @@ while True:
     
     # Draw Background
     display.fill("blue")
-    
-    player_group.update(delta_time)
+
     all_sprites.update(delta_time)
     
     # Draw Graphix
     all_sprites.draw(display)
-    player_group.draw(display)
     
     pygame.display.update()
